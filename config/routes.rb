8a8d91d726e733acfafadddf41a4933bc75b8247
Drew Krehel <drewkrehel@gmail.com>
2025-09-04 01:40:04 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
+  get "/", { :controller => "home", :action => "page" }
+  get "/dice/:number/:sides", { :controller => "dice", :action => "roll" }
 end

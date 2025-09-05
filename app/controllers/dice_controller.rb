ce98e28ed6fac1fc68fec9ef7db9af8df846c22a
Drew Krehel <drewkrehel@gmail.com>
2025-09-05 02:12:35 +0000

    seemd to work now, but formating is a little off.

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,18 +1,18 @@
 class DiceController < ApplicationController
-  def roll
+  # def roll
     # @num_die1 = params.fetch("die1").to_i
     # @num_die2 = params.fetch("die2").to_i
 
     # @dice = Array.new(@num_die1) {rand(1..@num_die2)}
 
-    def roll
+  def roll
     @number = params[:number].to_i
     @sides = params[:sides].to_i
     @rolls = Array.new(@number) { rand(1..@sides) }
-    @sum = @rolls.sum
+    # @sum = @rolls.sum
 
-    @outcome = "You rolled a total of #{@sum}."
+    # @outcome = "You rolled a total of #{@sum}."
 
-    render { :template => "dice_templates/outcome"}
+    render ({ :template => "dice_templates/outcome1"})
   end
 end

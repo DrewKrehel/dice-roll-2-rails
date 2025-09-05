81deae3ac1a4cf6b25a9b20a470e87f61c65d09c
Drew Krehel <drewkrehel@gmail.com>
2025-09-05 02:02:33 +0000

    using part of belles code for dice_controller and outcome.html.erb

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,10 +1,15 @@
 class DiceController < ApplicationController
   def roll
-    @num_die1 = params.fetch("die1").to_i
-    @num_die2 = params.fetch("die2").to_i
+    # @num_die1 = params.fetch("die1").to_i
+    # @num_die2 = params.fetch("die2").to_i
 
-    @dice = Array.new(@num_die1) {rand(1..@num_die2)}
-    @sum = @dice.sum
+    # @dice = Array.new(@num_die1) {rand(1..@num_die2)}
+
+    def roll
+    @number = params[:number].to_i
+    @sides = params[:sides].to_i
+    @rolls = Array.new(@number) { rand(1..@sides) }
+    @sum = @rolls.sum
 
     @outcome = "You rolled a total of #{@sum}."
 
